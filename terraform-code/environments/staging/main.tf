b83a4344763ed03accadeb2db437462743412a1a
Sukh <sukhdeepsona93@gmail.com>
2025-08-31 19:20:44 -0400

    New terraform folder structure and pipeline

--- terraform-code/environments/staging/main.tf
+++ terraform-code/environments/staging/main.tf
@@ -1,4 +1,4 @@
-# to call the main.tf file which is inside environments folder. The following is a mandatory step when using multi environments.
+# to call the main.tf file which is inside environments folder. The following is a mandatory step when using multi-environments.
 
 module "stack" {
   source = "../../stack"
