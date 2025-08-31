bad6d8e0796ab0e9688b107c4728f09605822c31
Sukh <sukhdeepsona93@gmail.com>
2025-08-31 17:44:57 -0400

    New terraform folder structure and pipeline -

--- terraform-code/stack/provider.tf
+++ terraform-code/stack/provider.tf
@@ -3,8 +3,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      #version = "~> 3.0"
-      version = "4.42.0"
+      version = "~> 3.0"
+      #version = "4.42.0"
     }
   }
 
