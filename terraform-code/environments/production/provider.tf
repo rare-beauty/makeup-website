8f24af06827f2b323c729291d97b0c4130b41a14
vinay pal <vinaypalvirk51@gmail.com>
2025-08-30 23:35:42 -0400

    terraform pipeline after registering storage provider

--- terraform-code/environments/production/provider.tf
+++ terraform-code/environments/production/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorage"
+    storage_account_name = "tfstatestorage30aug"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
--- terraform-code/environments/staging/provider.tf
+++ terraform-code/environments/staging/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorage123"
+    storage_account_name = "tfstatestorage30aug"
     container_name       = "tfstate"
     key                  = "staging.terraform.tfstate"
   }
