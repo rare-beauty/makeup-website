848c131389a4075bf1e5cffd293e6dfa07c7cdd9
vinay pal <vinaypalvirk51@gmail.com>
2025-09-01 16:29:16 -0400

    gitignore main.tf

--- terraform-code/stack/provider.tf
+++ terraform-code/stack/provider.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
   #subscription_id = var.cfg.subscription_id
 }
 
-data "azurerm_client_config" "current" {}
+#data "azurerm_client_config" "current" {}
 
 locals {
   tags = {
