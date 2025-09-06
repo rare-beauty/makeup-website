aaa84b21579f236695c4bc70b890ac7578db2792
vinay pal <vinaypalvirk51@gmail.com>
2025-09-06 19:50:13 -0400

    terraform modified pipeline

--- terraform-code/environments/production/variable.tf
+++ terraform-code/environments/production/variable.tf
@@ -1,4 +1,4 @@
-# Minimal root var so this folder accepts `cfg` from tfvars
+# Minimal root var so this folder accepts `cfg` from tfvars for staging & prod.
 variable "cfg" {
   description = "Env config"
   type        = any
