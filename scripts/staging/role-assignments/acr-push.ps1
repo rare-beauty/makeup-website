0b10c6c77ef624f11285f8993b2694bf86549138
Sukh <sukhdeepsona93@gmail.com>
2025-11-10 23:48:49 -0500

    checking

--- scripts/staging/role-assignments/acr-push.ps1
+++ scripts/staging/role-assignments/acr-push.ps1
@@ -1,8 +1,11 @@
 # === inputs (change RG if your ACR lives elsewhere) ===
-$SUB      = "545d4a9b-b402-4493-a10a-99d17130d280"
-$RG       = "staging-rgroup010"                # ACR's resource group
-$ACR_NAME = "stagingacr010"
-$STG_APPID  = "7e545c24-99fc-4ac5-8a03-a7916dde8bff"  # staging CI app (clientId)
+#$SUB      = "545d4a9b-b402-4493-a10a-99d17130d280"
+$SUB      = "4660c4f3-a83c-406e-8741-d1067921120b"
+$RG       = "staging-rgroup02"                # ACR's resource group
+$ACR_NAME = "stagingacr02"
+#$STG_APPID  = "7e545c24-99fc-4ac5-8a03-a7916dde8bff"  # staging CI app (clientId)
+$STG_APPID  = "53b0d08d-b186-40c2-8e4d-c9a3d3619e59"
+
 
 az account set --subscription $SUB
 
